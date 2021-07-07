368b88c1910e3227621a5f7901fba0df71a87fcd
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-07-07 14:36:08 +0100

    Fonts and colours

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Karla&family=Rubik:wght@700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
